edf002cabbdbe574ff09b27cc53547c266e0f2d3
Pedro Brito <pedrobrito14@gmail.com>
2026-03-12 15:43:39 +0000

    Config Update

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,6 +4,7 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/db"; // your drizzle instance
 import { dash } from "@better-auth/infra";
 export const auth = betterAuth({
+  appName: "SkinSpace", // Define your application name
   plugins: [expo(), dash()],
   emailAndPassword: {
     enabled: true, // Enable authentication using email and password.
@@ -12,4 +13,12 @@ export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg", // or "mysql", "sqlite"
   }),
+  experimental: {
+    joins: true, // Enable database joins for better performance
+  },
+  advanced: {
+    ipAddress: {
+      ipAddressHeaders: ["cf-connecting-ip", "x-forwarded-for"],
+    },
+  },
 });

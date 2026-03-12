90aa528a9b772514efcd03596ca7832af5b62e3f
Pedro Brito <pedrobrito14@gmail.com>
2026-03-12 15:10:05 +0000

    Add expo Support

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,14 @@
-import { betterAuth } from "better-auth";
-import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/db"; // your drizzle instance
+import { expo } from "@better-auth/expo";
+import { betterAuth } from "better-auth";
+
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
 export const auth = betterAuth({
+  plugins: [expo()],
+  emailAndPassword: {
+    enabled: true, // Enable authentication using email and password.
+  },
+  trustedOrigins: ["skinspaceapp://"],
   database: drizzleAdapter(db, {
     provider: "pg", // or "mysql", "sqlite"
   }),
